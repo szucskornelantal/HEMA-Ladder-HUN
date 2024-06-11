94dc756a312dc44088d01eae58073e8613ac92c4
szucskornelantal <szucskornel24@gmail.com>
2024-06-11 11:31:15 +0200

    Create LadderPage.vue

---
+++ src/pages/LadderPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page class="row items-top justify-evenly">
+    <q-tabs>
+      <q-route-tab
+        v-for="v in views"
+        :key="v"
+        :name="v"
+        :label="$t('view.' + v)"
+        :to="v"
+      />
+    </q-tabs>
+    <suspense :timeout="0">
+      <router-view />
+      <template #fallback>
+        <q-page class="row items-top justify-evenly"> Loading... </q-page>
+      </template>
+    </suspense>
+  </q-page>
+</template>
+
+<style lang="scss"></style>
+
+<script setup lang="ts">
+import { computed } from '@vue/reactivity';
+import { ComputedRef } from 'vue';
+import { View, viewReverseMap } from 'src/logic/ladder';
+
+const views: ComputedRef<View[]> = computed((): View[] => {
+  return Object.values(viewReverseMap);
+});
+</script>
